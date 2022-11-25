1f8a20d5d16c0d5058cbaca73b7c81e3fb8953de
pavaneshwar <pavaneshwar23@gmail.com>
2022-11-25 13:47:42 +0530

    deploying

---
+++ angular-uzumaki/src/app/cart.service.ts
@@ -0,0 +1,10 @@
+import { Product } from './products';
+import { Injectable } from '@angular/core';
+/* . . . */
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = [];
+/* . . . */
+}
